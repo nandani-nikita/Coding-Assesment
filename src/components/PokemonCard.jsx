37e245ce672de89bd74d5c68b68dec60b0aff96b
Nandani N <nikitann2501@gmail.com>
2021-12-17 16:04:57 +0530

    Pokemon Card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,7 +5,12 @@ const PokemonCard = ({ pokemon }) => {
         
         <img src={pokemon.images.large} alt={pokemon.name} />
         <h4>{pokemon.name}</h4>
+        Attacks: {(pokemon.attacks)? pokemon.attacks[0].name : "-"}
+        <br />
+        HP: {(pokemon.hp)? pokemon.hp : "-"}
+        <br />
         Abilities: {(pokemon.abilities)? pokemon.abilities[0].name : "N/A"}
+
       </div>
     );
   };
